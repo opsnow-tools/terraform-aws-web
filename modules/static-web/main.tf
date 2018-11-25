63237619bda21b082e6c16bed34a5967447ac70a
Jungyoul Yu <me@nalbam.com>
2018-11-25 07:35:42 -0800

    Update main.tf

--- modules/static-web/main.tf
+++ modules/static-web/main.tf
@@ -67,28 +67,28 @@ resource "aws_cloudfront_distribution" "default" {
     }
 
     compress = true
-    //viewer_protocol_policy = "allow-all"
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
+#     viewer_protocol_policy = "redirect-to-https"
 
     min_ttl = 0
     default_ttl = 3600
     max_ttl = 86400
   }
 
-  price_class = "PriceClass_All"
-  //price_class = "PriceClass_200"
+#   price_class = "PriceClass_All"
+  price_class = "PriceClass_200"
 
   restrictions {
-    //geo_restriction {
-    //  restriction_type = "none"
-    //  locations = []
-    //}
     geo_restriction {
-      restriction_type = "whitelist"
-      locations = [
-        "KR"
-      ]
+      restriction_type = "none"
+      locations = []
     }
+#     geo_restriction {
+#       restriction_type = "whitelist"
+#       locations = [
+#         "KR"
+#       ]
+#     }
   }
 
   viewer_certificate {
